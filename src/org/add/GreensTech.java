04e0e420962328e592c7295cd47fbd4304a8ce74
praneet4052 <praneetpraneet2000@gmail.com>
2022-03-14 19:46:16 +0530

    new class

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,28 @@
+package org.add;
+
+public class GreensTech {
+	
+public static void main(String[] args) {
+	
+	System.out.println(1);
+	System.out.println(2);
+	System.out.println(3);
+	System.out.println(4);
+	System.out.println(5);
+	
+	System.out.println(6/0);
+	String s1 = "pou";
+	try {
+		
+	} catch (ArithmeticException e) {
+		System.out.println("pou");
+	}
+	System.out.println(1);
+}
+	
+	
+	
+	
+	}
+
+
